6aab3afaf16e17145a52adea3fbcf8cbc219337f
smartptr <smartptr@sina.com>
2018-07-12 17:05:33 +0800

    加一些错误跟踪代码

--- FQC/Program.cs
+++ FQC/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace FQC
 {
@@ -13,9 +14,34 @@ namespace FQC
         [STAThread]
         static void Main()
         {
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+            Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(Application_ThreadException);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new PressureForm());//
+            Application.Run(new PressureForm());
         }
+
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("错误，CurrentDomain未捕捉到的异常!");
+            LogUnhandledException(e.ExceptionObject);
+        }
+
+        static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            MessageBox.Show("错误，Application未捕捉到的异常!");
+            LogUnhandledException(e.Exception);
+        }
+
+        static void LogUnhandledException(object exceptionobj)
+        {
+            //Log the exception here or report it to developer  
+            Exception e = ((Exception)exceptionobj);
+            MessageBox.Show(e.Message + "\nStackTrace=>" + e.StackTrace);
+
+        }
+
+        [DllImport("user32.dll", EntryPoint = "SendMessage")]
+        private static extern int SendMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
     }
 }
